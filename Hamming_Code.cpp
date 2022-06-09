dbf0f91c802e5cfe3bfca4eedb89e1b0be4f2817
FiqAtGit <82915997+binaryassasins@users.noreply.github.com>
2022-06-10 00:59:38 +0800

    Update Hamming_Code.cpp

--- Hamming_Code.cpp
+++ Hamming_Code.cpp
@@ -134,7 +134,7 @@ int main() {
 /**
 MIT License
 
-Copyright (c) [2022] [binaryassasins]
+Copyright (c) 2022 FiqAtGit
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
